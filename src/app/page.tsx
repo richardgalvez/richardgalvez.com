aaa638ba254ea9b0ff342803bfec169655b93999
Richard Galvez <richardalbertogalvez@gmail.com>
2026-06-13 23:00:56 -0400

    Updated .gitignore, got text from current temp site in main page.tsx.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
 export default function Home() {
   return (
-    <div> Hello world.
+    <div>
+      <body>
+        <body>
+          <div>
+            <h1>Richard Galvez</h1>
+              <h2>Senior Software Engineer 💻</h2>
+                <p>Based in New York, NY.</p>
+                <p>This site might give you a nice early 2000s internet vibe, but it&#39;s just under construction.</p>
+                <p>In the meantime, check out my
+                  <a href="https://www.linkedin.com/in/richardalbertogalvez"> LinkedIn</a> or 
+                  <a href="https://github.com/richardgalvez"> GitHub</a> page.
+                </p>
+          </div>
+        </body>
+      </body>
     </div>
   );
 }
