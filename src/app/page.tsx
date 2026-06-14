12b20fcc344dd9c7bde2c66158d429ade5ecb934
Richard Galvez <richardalbertogalvez@gmail.com>
2026-06-14 00:31:54 -0400

    Main text and links set up. Styling WIP.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
 export default function Home() {
   return (
     <div>
-      <body>
-        <body>
-          <div>
-            <h1>Richard Galvez</h1>
-              <h2>Senior Software Engineer 💻</h2>
-                <p>Based in New York, NY.</p>
-                <p>This site might give you a nice early 2000s internet vibe, but it&#39;s just under construction.</p>
-                <p>In the meantime, check out my
-                  <a href="https://www.linkedin.com/in/richardalbertogalvez"> LinkedIn</a> or 
-                  <a href="https://github.com/richardgalvez"> GitHub</a> page.
-                </p>
-          </div>
-        </body>
-      </body>
+      <div className="box-align">
+        <div className="presentation-box">
+          <h1 className="h1-primary">Richard Galvez</h1>
+            <h2 className="h2-primary">Senior Software Engineer 💻</h2>
+              <p className="p-primary">Based in New York, NY.<br />
+              This site might give you a nice early 2000s internet vibe, but it&#39;s just under construction.<br />
+              In the meantime, check out my <a href="https://www.linkedin.com/in/richardalbertogalvez" className="a-link">LinkedIn</a> or <a href="https://github.com/richardgalvez" className="a-link">GitHub</a> page.
+              </p>
+        </div>
+      </div>
     </div>
   );
 }
