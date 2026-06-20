a7fdab9dc41749a75128284d0ab17ba50b3d02d9
Richard Galvez <richardalbertogalvez@gmail.com>
2026-06-19 23:08:32 -0400

    Updated text and box styling for page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ export default function Home() {
   return (
     <div>
       <div className="box-align">
-        <div className="presentation-box">
+        <div className="presentation-box shadow-2xl/70">
+          <div className="text-center font-rsans">
           <h1 className="h1-primary">Richard Galvez</h1>
             <h2 className="h2-primary">Senior Software Engineer 💻</h2>
               <p className="p-primary">Based in New York, NY.<br />
               This site might give you a nice early 2000s internet vibe, but it&#39;s just under construction.<br />
-              In the meantime, check out my <a href="https://www.linkedin.com/in/richardalbertogalvez" className="a-link">LinkedIn</a> or <a href="https://github.com/richardgalvez" className="a-link">GitHub</a> page.
+              In the meantime, check out my <a href="https://www.linkedin.com/in/richardalbertogalvez" className="a-link text-indigo-800">LinkedIn</a> or <a href="https://github.com/richardgalvez" className="a-link text-indigo-800">GitHub</a> page.
               </p>
+          </div>
         </div>
       </div>
     </div>
